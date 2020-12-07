d7c8a3788be997a4ddaa46d55b272fd1adf38506
Katherine Lamoureux <katherine.lamoureux@gmail.com>
2020-12-07 12:07:23 -0500

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMoter))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    void Start ()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update ()
+    {
+        // Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.right * _zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        // Apply movement
+        motor.Move(_velocity);
+
+    }
+
+}
