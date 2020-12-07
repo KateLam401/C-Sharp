8d15def7f66dfcb64b252b5bd89ebf0e4fe7dc6d
Katherine Lamoureux <katherine.lamoureux@gmail.com>
2020-12-07 12:11:06 -0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Interact()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Interact();
+        // Attack the enemy
     }
 }
