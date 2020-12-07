d8485d3093104ce574be31e3927d8266c7b06a43
Katherine Lamoureux <katherine.lamoureux@gmail.com>
2020-12-07 12:06:14 -0500

    Create PlayerSetup.cs

---
+++ PlayerSetup.cs
@@ -0,0 +1,19 @@
+/*
+
+Player
+-sphere mesh (remove)
+-sphere collider
+-mesh renderer (remove)
+-rigid body (add)
+
+>> Graphics
+>>> Body
+-sphere mesh
+-sphere collider (remove)
+-mesh renderer
+
+So you end up with:
+>Player (Collider Layer)
+>>Graphics (Mesh Layer)
+
+*/
